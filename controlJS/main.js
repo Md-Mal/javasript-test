4c31131b641ef7f1b97d22a5b5921e8a9e53cae5
kabota <kmlzsop@gmail.com>
2020-05-12 03:55:37 +0800

    :)

--- controlJS/main.js
+++ controlJS/main.js
@@ -4,6 +4,8 @@ let inputs = document.querySelectorAll(".control input")
 
 function updateChange(){
     let suffix = this.dataset.size || ""
+
+    //to get name of attribute and change it 
     document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix);
 }
 
---
+++ controlJS/readme.md
@@ -0,0 +1,1 @@
+**this is a contor slider change event dom**
